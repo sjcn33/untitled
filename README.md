6a45c3e554cb8e789a889bbf304ffef41cb14a70
SJCN33 <sjcn33@gmail.com>
2022-03-19 12:56:58 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # untitled
+[![Kotlin](https://img.shields.io/badge/java-17-ED8B00.svg?logo=java)](https://www.azul.com/)
+[![Kotlin](https://img.shields.io/badge/kotlin-1.6.10-585DEF.svg?logo=kotlin)](http://kotlinlang.org)
+[![GitHub](https://img.shields.io/github/license/sjcn33/untitled)](https://www.gnu.org/licenses/gpl-3.0.html)
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,13 +16,13 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 
     implementation(kotlin("stdlib"))
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.mockito:mockito-core:4.2.0")
+    testImplementation("org.mockito:mockito-core:4.3.1")
 }
 
 project.extra.set("packageName", name.replace("-", ""))
